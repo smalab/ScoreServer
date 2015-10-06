4b99dad087105e373a0ae67afb7b24114d110121
oitsmalab <oitsmalab@gmail.com>
2015-10-06 14:43:38 +0900

    問１と問２のシーンを作成したが、シーン遷移のスクリプトをまだ書けていない。

---
+++ TestN/Assets/BottunClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottunClick : MonoBehaviour
+{
+    [SerializeField]
+    UnityEngine.Events.UnityEvent ev;
+
+    //シーンNodatest_２へ遷移
+    Application.LoadLevel("Nodatest_2")
+
+        }
