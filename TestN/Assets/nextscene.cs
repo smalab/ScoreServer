b31e6172f59c1d48746a2c8ecbc01d2cd7a150d7
oitsmalab <oitsmalab@gmail.com>
2015-10-09 14:44:24 +0900

    タイトル、問題文の作成が完了し、タイトルから最後の問題文までのシーン遷移が完了した。 タイトル画面の学年記入欄にドロップダウンを使用しているが、動作していない　10/9　14：50

--- TestN/Assets/nextscene.cs
+++ TestN/Assets/nextscene.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class nextscene : MonoBehaviour
 {
+    public int next = 1;
     public void ClickTest()
     {
-        Application.LoadLevel("Nodatest_2");
+        Application.LoadLevel(next);
         Debug.Log("Clicked.");
+       
     }
 }
