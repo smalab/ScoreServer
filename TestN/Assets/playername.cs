7f9a4f4e2614a6772c0f9d3b823feb4dd2b25d08
oitsmalab <oitsmalab@gmail.com>
2015-10-15 15:30:17 +0900

    タイトル画面で名前を入力してスタートボタンを押すと、入力した名前が保存され別のテキストに表示されるようになった。　10/15　15：30

---
+++ TestN/Assets/playername.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playername : MonoBehaviour {
+    string pn;
+	// Use this for initialization
+	void Start () {
+        pn = textsave.player();
+        pn.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TestN/Assets/textsave.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class textsave : MonoBehaviour {
+
+   public static string player;
+    public InputField inputField;
+    public Text text;
+
+    public void save () {
+       textsave.player = inputField.text;
+        text.text = textsave.player;
+        inputField.text = "";
+    }
+    }
