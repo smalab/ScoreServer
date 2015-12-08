ad21cbddcf327d1e73b83d55dc1ca844f2470a96
oitsmalab <oitsmalab@gmail.com>
2015-12-08 14:40:34 +0900

    IRT計算スクリプトの修正（（）の箇所が悪かったので）

--- TestN/Assets/IRT_e.cs
+++ TestN/Assets/IRT_e.cs
@@ -34,81 +34,87 @@ public class IRT_e: MonoBehaviour {
 
 	
 	//最大尤度
-	public static double max=-5;
+	public static double max=0;
 	public static double maxLevel=-5;
 	//最大尤度推定用変数
 	public static double MLE;
 	public static float Pi=Mathf.PI;
 	void Start(){
 		
-		for (double i=-5.0; i<5.5; i=i+0.1) {
+		for (double i=-5.0; i<=5.0; i=i+0.1) {
 			
-			Q1_1icct=1/(1+Mathf.Pow(Pi,(float)(-1*(i-Q1_1dif))));
-			if(AnsQ1_e.q1 != 1){
+			Q1_1icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_1dif))));
+			if(AnsQ1_e.q1 == 0){
 				Q1_1icct=1-Q1_1icct;
 			}
-			
-			Q1_2icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_2dif))));
-			if(AnsQ1_e.q2 != 1)
-				Q1_2icct=1-Q1_2icct;
 
-			Q1_3icct=1/(1+Mathf.Pow(Pi,(float)(-1*(i-Q1_3dif))));
-			if(AnsQ1_e.q3 != 1){
+			Q1_2icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_2dif))));
+			if(AnsQ1_e.q2 == 0){
+				Q1_2icct=1-Q1_2icct;
+			}
+
+			Q1_3icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_3dif))));
+			if(AnsQ1_e.q3 == 0){
 				Q1_3icct=1-Q1_3icct;
 			}
 			
 			Q1_4icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_4dif))));
-			if(AnsQ1_e.q4 != 1)
+			if(AnsQ1_e.q4 == 0){
 				Q1_4icct=1-Q1_4icct;
+			}
 
-			Q1_5icct=1/(1+Mathf.Pow(Pi,(float)(-1*(i-Q1_5dif))));
-			if(AnsQ1_e.q5 != 1){
+			Q1_5icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_5dif))));
+			if(AnsQ1_e.q5 == 0){
 				Q1_5icct=1-Q1_5icct;
 			}
 			
 			Q1_6icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_6dif))));
-			if(AnsQ2_e.q1 != 1)
+			if(AnsQ2_e.q1 == 0){
 				Q1_6icct=1-Q1_6icct;
+			}
 
-			Q1_7icct=1/(1+Mathf.Pow(Pi,(float)(-1*(i-Q1_7dif))));
-			if(AnsQ2_e.q2 != 1){
+			Q1_7icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_7dif))));
+			if(AnsQ2_e.q2 == 0){
 				Q1_7icct=1-Q1_7icct;
 			}
 			
 			Q1_8icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_8dif))));
-			if(AnsQ2_e.q3 != 1)
+			if(AnsQ2_e.q3 == 0){
 				Q1_8icct=1-Q1_8icct;
+			}
 
-			Q1_9icct=1/(1+Mathf.Pow(Pi,(float)(-1*(i-Q1_9dif))));
-			if(AnsQ2_e.q4 != 1){
+			Q1_9icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_9dif))));
+			if(AnsQ2_e.q4 == 0){
 				Q1_1icct=1-Q1_1icct;
 			}
 
 			Q2_1icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q2_1dif))));
-			if(AnsQ3_e.q1 != 1)
+			if(AnsQ3_e.q1 == 0){
 				Q2_1icct=1-Q2_1icct;
-			
+			}
 
 			Q3_1icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q3_1dif))));
-			if(AnsQ3_e.q2 != 1)
+			if(AnsQ3_e.q2 == 0){
 				Q3_1icct=1-Q3_1icct;
-			
+			}
 
 			
 			Q4_1icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q4_1dif))));
-			if(AnsQ4_e.q1 != 1)
+			if(AnsQ4_e.q == 0){
 				Q4_1icct=1-Q4_1icct;		
-			
+			}
 
 			
 
 			MLE=Q1_1icct*Q1_2icct*Q1_3icct*Q1_4icct*Q1_5icct*Q1_6icct*Q1_7icct*Q1_8icct*Q1_9icct*Q2_1icct*Q3_1icct*Q4_1icct;
 			if(MLE>max){
 				max=Mathf.Max((float)max,(float)MLE);
-				maxLevel=Mathf.Max((float)maxLevel,(float)i);
-				Debug.Log (MLE);
-				Debug.Log (i);
+				maxLevel=i;
+	
+
 			}
+				Debug.Log (MLE);
+			Debug.Log (i);
 		}
 		textmax = maxLevel.ToString ();
 		maxIRT.text = textmax;
--- TestN/Assets/IRT_h.cs
+++ TestN/Assets/IRT_h.cs
@@ -2,29 +2,20 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class IRT_b: MonoBehaviour {
+public class IRT_h: MonoBehaviour {
 	public Text maxIRT;
 	public string textmax;
 	//問題の難易度
-	public static double Q1_1dif=0.310;
-	public static double Q1_2dif=-0.723;
-	public static double Q2_1dif=-0.985;
-	public static double Q2_2dif=0.256;
-	public static double Q3_1dif=-0.519;
-	public static double Q3_2dif=0.704;
-	public static double Q4_1dif=-1.956;
-	public static double Q4_2dif=-0.213;
-	public static double Q5dif=3.423;
-	//問題の識別度
-	public static double Q1_1se=0.247;
-	public static double Q1_2se=0.241;
-	public static double Q2_1se=0.245;
-	public static double Q2_2se=0.246;
-	public static double Q3_1se=0.239;
-	public static double Q3_2se=0.257;
-	public static double Q4_1se=0.287;
-	public static double Q4_2se=0.240;
-	public static double Q5se=0.443;
+	public static double Q1_1dif=1.155;
+	public static double Q1_2dif=-0.996;
+	public static double Q2_1dif=-0.335;
+	public static double Q2_2dif=-0.176;
+	public static double Q3_1dif=-1.359;
+	public static double Q3_2dif=0.302;
+	public static double Q4_1dif=-0.658;
+	public static double Q4_2dif=0.142;
+	public static double Q5dif=2.006;
+
 	//1PLのICC計算(正答確率）
 	public static float Q1_1icct;
 	public static double Q1_2icct;
@@ -35,61 +26,61 @@ public class IRT_b: MonoBehaviour {
 	public static double Q4_1icct;
 	public static double Q4_2icct;
 	public static double Q5icct;
-
+	
 	//最大尤度
-	public static double max=-5;
+	public static double max=0;
 	public static double maxLevel=-5;
 	//最大尤度推定用変数
 	public static double MLE;
-	public static float Pi=Mathf.PI;
-	 void Start(){
-
+	void Start(){
+		
 		for (double i=-5.0; i<=5.0; i=i+0.1) {
-
-			Q1_1icct=1/(1+Mathf.Pow(Pi,(float)(-1*(i-Q1_1dif))));
-			if(AnsQ1.q1 != 1){
-			Q1_1icct=1-Q1_1icct;
+			
+			Q1_1icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_1dif))));
+			if(AnsQ1_h.q1 != 1){
+				Q1_1icct=1-Q1_1icct;
 			}
-	
+			
 			Q1_2icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q1_2dif))));
-			if(AnsQ1.q2 != 1)
+			if(AnsQ1_h.q2 != 1)
 				Q1_2icct=1-Q1_2icct;
-
+			
 			Q2_1icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q2_1dif))));
-			if(AnsQ2.q1 != 1)
-			Q2_1icct=1-Q2_1icct;
-
+			if(AnsQ2_h.q1 != 1)
+				Q2_1icct=1-Q2_1icct;
+			
 			Q2_2icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q2_2dif))));
-			if(AnsQ2.q2 != 1)
-			Q2_2icct=1-Q2_2icct;
-
+			if(AnsQ2_h.q2 != 1)
+				Q2_2icct=1-Q2_2icct;
+			
 			Q3_1icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q3_1dif))));
-			if(AnsQ3.q1 != 1)
-			Q3_1icct=1-Q3_1icct;
-
+			if(AnsQ3_h.q1 != 1)
+				Q3_1icct=1-Q3_1icct;
+			
 			Q3_2icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q3_2dif))));
-			if(AnsQ3.q2 != 1)
-			Q3_2icct=1-Q3_2icct;
-
+			if(AnsQ3_h.q2 != 1)
+				Q3_2icct=1-Q3_2icct;
+			
 			Q4_2icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q4_2dif))));
-			if(AnsQ4.q2 != 1)
-			Q4_2icct=1-Q4_2icct;		
-
+			if(AnsQ4_h.q2 != 1)
+				Q4_2icct=1-Q4_2icct;		
+			
 			Q4_1icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q4_1dif))));
-			if(AnsQ4.q1 != 1)
-			Q4_1icct=1-Q4_1icct;
-
+			if(AnsQ4_h.q1 != 1)
+				Q4_1icct=1-Q4_1icct;
+			
 			Q5icct=1/(1+Mathf.Pow(Mathf.PI,(float)(-1*(i-Q5dif))));
-			if(AnsQ5.q5Ans != 1)
-			Q5icct=1-Q5icct;
+			if(AnsQ5_h.q5 != 1)
+				Q5icct=1-Q5icct;
+
 			MLE=Q1_1icct*Q1_2icct*Q2_1icct*Q2_2icct*Q3_1icct*Q3_2icct*Q4_1icct*Q4_2icct*Q5icct;
 			if(MLE>max){
-			max=Mathf.Max((float)max,(float)MLE);
-			maxLevel=Mathf.Max((float)maxLevel,(float)i);
+				max=Mathf.Max((float)max,(float)MLE);
+				maxLevel=Mathf.Max((float)maxLevel,(float)i);
 			}
 			Debug.Log (MLE);
 			Debug.Log (i);
-	}
+		}
 		textmax = maxLevel.ToString ();
 		maxIRT.text = textmax;
 		Debug.Log (max);
