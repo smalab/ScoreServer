94434a2a7e0869089fef4822e263d72a40dcf215
oitsmalab <oitsmalab@gmail.com>
2016-01-12 14:51:04 +0900

    データストアからデータを取得し、unity内の変数に格納することに成功

--- TestN/Assets/NCMBpush.cs
+++ TestN/Assets/NCMBpush.cs
@@ -1,36 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Threading;
 using NCMB;
 using System.Collections.Generic;
 
 public class NCMBpush : MonoBehaviour {
 	public  string q;
+
 	// Use this for initialization
 	void Start () {
+
 		NCMBObject testClass = new NCMBObject ("TestClass");
+		NCMBObject KKK = new NCMBObject ("TestClass");
+
 		testClass ["message"] = "Hello, NCMB!";
 		testClass ["name"] = textsave.player;
 		testClass ["glade"] = textsave.glade;
 		testClass.SaveAsync ();
 
-		//TestClasssを検索するクラスを作成
-		//NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject> ("TestClass");
-		//gladeの値が3と一致するオブジェクト検索
-		//query.WhereEqualToで検索条件を設定
-		//query.WhereEqualTo ("glade", 5);
-		//query.FindAsync ((List<NCMBObject> objList ,NCMBException e) => {
-		//	if (e != null) {
+		KKK.ObjectId = "gYXqokSGFFXs9Ci";
+		KKK.FetchAsync ((NCMBException e) => {
+			if (e != null) {
 				//検索失敗時の処理
-		//		Debug.Log ("×");
-		//	} else {
-				//gladeが3のオブジェクトを出力
-		//		foreach (NCMBObject obj in objList) {
-		//			Debug.Log ("objectId:" + obj.ObjectId);
-		//			q = obj.ObjectId;
-		//		}
-		//	}
-		//});
-	
+			} else {
+			}
+
+		});
+		Thread.Sleep (3000);
+		q = (string)KKK["name"];
+		Debug.Log (q);
            
 	}
 	
